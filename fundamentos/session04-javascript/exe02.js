b3ef85e1252204c9736a8ad353f1f0076c787bca
Luanderson Alves <luaoderson@gmail.com>
2022-11-23 19:32:02 -0300

    somes exersisses

---
+++ fundamentos/session04-javascript/exe02.js
@@ -0,0 +1,13 @@
+let a = 10, b = 20;
+
+let adicao = (a + b);
+let subtracao = (a - b);
+let divisao = (a / b);
+let multiplicacao = (a * b);
+let modulo = (a % b);
+
+console.log(adicao);
+console.log(subtracao);
+console.log(divisao);
+console.log(multiplicacao);
+console.log(modulo);
