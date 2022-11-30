0860102b2252599ababe47928a11ef7c56a5e62f
Luanderson Alves <luaoderson@gmail.com>
2022-11-30 20:13:34 -0300

    exercicios 4, 5, 6, 7 e 8

---
+++ fundamentos/session04-javascript/day04/exe05.js
@@ -0,0 +1,51 @@
+/* Usando o objeto abaixo, faça os exercícios a seguir:
+
+    let info = {
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    };
+
+    Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+    Margarida e Tio Patinhas
+    Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+    Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+    Ambos recorrentes // Atenção para essa última linha!
+
+*/
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info['recorrente'] = 'sim';
+
+let infoTwo = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+};
+infoTwo['recorrente'] = 'sim';
+
+let infos = [];
+let infosTwo = [];
+
+for(let key in info){
+  infos.push(info[key]);
+}
+
+for(let keyTwo in infoTwo){
+  infosTwo.push(infoTwo[keyTwo]);
+}
+
+for(let index = 0; index < infos.length; index += 1){
+  if(infos[index] === infosTwo[index]){
+    console.log('Ambos recorrentes');
+  }else{
+    console.log(infos[index], 'e', infosTwo[index]);
+  }
+
+}
+// INCOMPLETOOOOOOOOOOOOOOOOOOOOOO
