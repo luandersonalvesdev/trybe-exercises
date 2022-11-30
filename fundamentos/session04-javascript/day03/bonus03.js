242dc8c0148242d5b4b7fc1ab52da11f1c2255b1
Luanderson Alves <luaoderson@gmail.com>
2022-11-29 23:59:15 -0300

    exercicio bonus 3 em andamento

---
+++ fundamentos/session04-javascript/day03/bonus03.js
@@ -0,0 +1,31 @@
+/* Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+Agora inverta o lado do triângulo. Por exemplo:
+
+n = 5
+
+    *
+   **
+  ***
+ ****
+*****
+Atenção! Note que esse exercício é bem mais complexo que o anterior! Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também.
+*/
+
+let n = 5, writer = '', assistent = n;
+
+while(n != 0){
+
+  for(let index = 0; index < assistent; index++){
+
+    if((index+1) == assistent){
+      writer += '*';
+    }else{
+      writer += '-';
+    }
+  }
+
+  console.log(writer);
+  writer = '';
+  n--;
+}
