46db762d0f5902204b529ae8f9da54cfc8582032
Luanderson Alves <luaoderson@gmail.com>
2022-12-12 14:48:54 -0300

    exercises

--- fundamentos/session05-js-dom-eventos-seletores/day02/conteudo/03.js
+++ fundamentos/session05-js-dom-eventos-seletores/day02/conteudo/03.js
@@ -5,3 +5,8 @@ let childWhereIAm = whereIAm.firstElementChild;
 
 whereIAm.removeChild(childWhereIAm); // REMOVENDO FILHO DIRETAMENTE DO PAI
 whereIAm.firstElementChild.remove(); // REMOVENDO A SI PROPRIO
+
+// VENDO QUANDO RETORNO DO classList
+let section = document.querySelector('#pai');
+
+console.log(section.classList);
