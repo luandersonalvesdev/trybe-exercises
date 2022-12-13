641b6e0f000902706f943a76bcea7f8d3e6fd013
Luanderson Alves <luaoderson@gmail.com>
2022-12-13 12:35:53 -0300

    part 05

--- fundamentos/session05-js-dom-eventos-seletores/day03/exercicio/script.js
+++ fundamentos/session05-js-dom-eventos-seletores/day03/exercicio/script.js
@@ -15,6 +15,7 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const fridays = [4, 11, 18, 25];
 
 // SELECT
 const ulDays = document.querySelector('.days-container #days');
@@ -24,17 +25,17 @@ divButton = document.querySelector('.buttons-container');
 const createButtonHoliday = document.createElement('button');
 const createButtonFriday = document.createElement('button');
 
-
+// ADICIONANDO CLASSES AOS DIAS
 decemberDaysList.forEach((value) => {
   const li = document.createElement('li');
-  li.innerHTML = value;
+  li.innerText = value;
   li.className = 'day'
 
-  if (li.innerHTML == 24 || li.innerHTML == 31) {
+  if (li.innerText == 24 || li.innerText == 31) {
     li.className = 'day holiday'
   };
 
-  if (li.innerHTML == 4 || li.innerHTML == 11 || li.innerHTML == 18) {
+  if (li.innerText == 4 || li.innerText == 11 || li.innerText == 18) {
     li.className = 'day friday'
   };
 
@@ -45,6 +46,7 @@ decemberDaysList.forEach((value) => {
   ulDays.appendChild(li);
 });
 
+// CRIANDO O BOTÃO FERIADO
 const createButtonHolidayFunction = (feriados) => {
   createButtonHoliday.innerText = feriados;
   createButtonHoliday.id = 'btn-holiday';
@@ -53,19 +55,23 @@ const createButtonHolidayFunction = (feriados) => {
 };
 createButtonHolidayFunction('Feriados');
 
-
+// ALTERAR COR DOS FERIADOS
 createButtonHoliday.addEventListener('click', () => {
+  const colorChange = 'rgb(200, 200, 222)'
+  const colorDefault = '#eee'
+
   for (let value of ulDays.children) {
     if (value.classList.contains('holiday')) {
-      if (value.style.backgroundColor === 'rgb(200, 200, 222)') {
-        value.style.backgroundColor = '#eee'
+      if (value.style.backgroundColor === colorChange) {
+        value.style.backgroundColor = colorDefault;
       } else {
-        value.style.backgroundColor = '#C8C8DE'
+        value.style.backgroundColor = colorChange;
       }
     }
   }
 });
 
+// CRIANDO O BOTÃO SEXTA
 const createButtonFridayFunction = (sexta) => {
   createButtonFriday.id = 'btn-friday';
   createButtonFriday.innerText = sexta;
@@ -73,3 +79,22 @@ const createButtonFridayFunction = (sexta) => {
   divButton.appendChild(createButtonFriday);
 };
 createButtonFridayFunction('Sexta-Feira');
+
+// ALTERAR TEXTO DOS DIAS DE SEXTA
+createButtonFriday.addEventListener('click', (event) => {
+  const textChange = 'Sextou';
+  let aux = 0;
+
+  for (let index = 0; index < ulDays.children.length; index += 1) {
+    if (ulDays.children[index].classList.contains('friday')) {
+      if (ulDays.children[index].innerText === textChange) {
+        console.log(ulDays.children[index].innerText);
+        ulDays.children[index].innerText = fridays[aux];
+      } else {
+        console.log(ulDays.children[index].innerText);
+        ulDays.children[index].innerText = textChange;
+      }
+      aux += 1;
+    };
+  }
+});
