4fae3a8bef4be2f3db25ebd1769b1f28ebeaba91
Luanderson Alves <luaoderson@gmail.com>
2022-12-13 00:52:38 -0300

    part 01

--- fundamentos/session05-js-dom-eventos-seletores/day03/exercicio/script.js
+++ fundamentos/session05-js-dom-eventos-seletores/day03/exercicio/script.js
@@ -14,3 +14,22 @@ const createDaysOfTheWeek = () => {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const ulDays = document.querySelector('.days-container #days');
+
+decemberDaysList.forEach((value) => {
+  const li = document.createElement('li');
+  li.innerHTML = value;
+  li.className = 'day'
+
+  if (li.innerHTML == 24 || li.innerHTML == 25 || li.innerHTML == 31) {
+    li.className = 'day holiday'
+  };
+
+  if (li.innerHTML == 4 || li.innerHTML == 11 || li.innerHTML == 18 || li.innerHTML == 25) {
+    li.className = 'day friday'
+  };
+
+  ulDays.appendChild(li);
+});
