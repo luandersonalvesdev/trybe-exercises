2bcf109f8e1a5ca9645e86bd6157aa71f471f81d
Luanderson Alves <luaoderson@gmail.com>
2022-12-13 22:43:44 -0300

    somes css things and initial js

--- fundamentos/session05-js-dom-eventos-seletores/day04/exercicio/script.js
+++ fundamentos/session05-js-dom-eventos-seletores/day04/exercicio/script.js
@@ -1,4 +1,13 @@
-/* RESET */
-* {
+const buttonsColorBg = ['white', 'black', 'green', 'blue', 'yellow'];
+const buttonsColorFont = ['black', 'red', 'white'];
+const buttonsFontSize = ['8pt', '10pt', '12pt', '14pt', '20pt'];
+const buttonsLineSpacing = ['1', 'normal', '1.5', '2.0', '3.0'];
+const buttonsFontType = ['Arial', 'Times New Roman'];
 
-}
+// SELECT FROM HTML
+const ulColorBg = document.querySelector('.color-background');
+const ulColorFont = document.querySelector('.color-font');
+const ulFontSize = document.querySelector('.font-size');
+const ulLineSpacing = document.querySelector('.line-spacing');
+const ulFontType = document.querySelector('.font-type');
+
